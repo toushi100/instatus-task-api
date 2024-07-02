c6d712044b379ea5f7931313a5ac04c6cca6e9da
Ahmed Salah <toushi2009@gmail.com>
2024-07-02 16:46:31 +0300

    feat: Add indexEvent endpoint to AppService

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,9 +5,26 @@ import { Prisma, event } from '@prisma/client';
 @Injectable()
 export class AppService {
   constructor(private readonly prisma: PrismaService) {}
-  createEvent(createEventData: Prisma.eventCreateInput) {
+  createEvent(createEventData: Prisma.eventCreateInput): Promise<event> {
     return this.prisma.event.create({
       data: createEventData,
+    });
+  }
+
+  indexEvent(params: {
+    skip?: number;
+    take?: number;
+
+    where?: Prisma.eventWhereInput;
+    orderBy?: Prisma.eventOrderByWithRelationInput;
+  }): Promise<event[]> {
+    const { skip, take, where, orderBy } = params;
+
+    return this.prisma.event.findMany({
+      skip,
+      take,
+      where,
+      orderBy,
     });
   }
 }
