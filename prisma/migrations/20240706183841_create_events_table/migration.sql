efe141ca0149dfb3da66fb66122e865a0faeb14d
Ahmed Salah <toushi2009@gmail.com>
2024-07-06 21:39:05 +0300

    Refactor database provider to use PostgreSQL instead of SQLite

--- prisma/migrations/20240706183841_create_events_table/migration.sql
+++ prisma/migrations/20240706183841_create_events_table/migration.sql
@@ -1,11 +1,13 @@
 -- CreateTable
 CREATE TABLE "Event" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "occurred_at" DATETIME NOT NULL,
+    "id" SERIAL NOT NULL,
+    "occurred_at" TIMESTAMP(3) NOT NULL,
     "actor_email" TEXT NOT NULL,
     "actor_name" TEXT NOT NULL,
     "actor_id" TEXT NOT NULL,
     "action_name" TEXT NOT NULL,
     "action_id" TEXT NOT NULL,
-    "target_id" TEXT NOT NULL
+    "target_id" TEXT NOT NULL,
+
+    CONSTRAINT "Event_pkey" PRIMARY KEY ("id")
 );
